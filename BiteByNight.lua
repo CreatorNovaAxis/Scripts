82b0e3a32b1adc1b91d49f095aa68b41f269ef5a
CreatorNovaAxis <analyzpodr@gmail.com>
2026-05-02 01:47:56 +0500

    Update BiteByNight.lua

--- BiteByNight.lua
+++ BiteByNight.lua
@@ -1,1 +1,3 @@
+getgenv().SCRIPT_KEY = "KEYLESS"
+
 loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/bba67614813555e540a369a28d6411e5cf09b6ef881839a9952efe97f15913d1/download"))()
