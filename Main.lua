c0a38f7ad424475d44e504ffc61ba4f71d51c1d2
CreatorNovaAxis <analyzpodr@gmail.com>
2026-05-02 15:44:20 +0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,3 +1,3 @@
 script_key = "Trial"
 
-loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/668cc204ab1a06f2069f7db6613f610e0f313b0b422eb4b408feb85f1fa7d155.lua"))()
+loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/b7ccfd9a6da76ea02f64165edb4f156be8a5e22d9d6d26611afbd7a9ebea3b27.lua"))()
